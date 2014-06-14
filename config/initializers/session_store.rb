c2a09c7d8505fe7e74cbc32c108943634dabfce8
nobuyasu <nobuyasu@isao-mac-air.local>
2014-06-14 23:30:56 +0900

    Finish demo app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_demo_app_session'
+DemoApp::Application.config.session_store :cookie_store, key: 'df329d8fdc6f739b4be450942610eeb2d62a741b9436a969eca858b63a8609818fe9929959ad6e5fb69b2c87b8c7f2d1684779a12e880cf0824ad5267a8a6dd5'
